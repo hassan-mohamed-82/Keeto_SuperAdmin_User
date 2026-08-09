ca4762d48053f940eeb894b503f49c821c9c8af0
basantsaed2 <basantsaed718@gmail.com>
2026-08-09 10:31:47 +0300

    edit

--- src/routes/admin/order.ts
+++ src/routes/admin/order.ts
@@ -3,7 +3,9 @@ import { catchAsync } from "../../utils/catchAsync";
 import { getOrdersByRestaurant, getOrderDetails, getAllOrders } from "../../controllers/admin/order";
 import { hasPermission } from "../../middlewares/";
 const router = Router();
+
+router.get("/all", hasPermission("Orders", "View"), catchAsync(getAllOrders));
+
 router.get("/:restaurantId", hasPermission("Orders", "View"), catchAsync(getOrdersByRestaurant));
 router.get("/:restaurantId/:orderId", hasPermission("Orders", "View"), catchAsync(getOrderDetails));
-router.get("/all", hasPermission("Orders", "View"), catchAsync(getAllOrders));
 export default router;
