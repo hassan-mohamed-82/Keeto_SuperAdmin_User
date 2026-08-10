de0305eb183d77705d96d4becdf27ee5b6f59c82
basantsaed2 <basantsaed718@gmail.com>
2026-08-10 12:34:03 +0300

    add location in address schem of user

--- src/controllers/user/address.ts
+++ src/controllers/user/address.ts
@@ -19,7 +19,7 @@ export const addUserAddress = async (req: Request, res: Response) => {
     try {
         if (!req.user) throw new UnauthorizedError("Unauthenticated");
         const userId = req.user.id;
-        const { lat, lng, type, title, street, number, floor, zoneId, landmark  } = req.body;
+        const { lat, lng, type, title, street, number, floor, zoneId, landmark,location  } = req.body;
 
         const newAddress = await db.insert(addresses).values({
             id: uuidv4(),
@@ -33,6 +33,7 @@ export const addUserAddress = async (req: Request, res: Response) => {
             zoneId,
             floor,
             landmark: landmark || null,
+            location: location || null,
         });
 
         return SuccessResponse(res, { message: "Address added successfully", data: newAddress });
@@ -62,7 +63,7 @@ export const updateUserAddress = async (req: Request, res: Response) => {
     if (!req.user) throw new UnauthorizedError("Unauthenticated");
     const userId = req.user.id;
     const { addressId } = req.params;
-    const { lat, lng, type, title, street, number, floor, zoneId, landmark } = req.body;
+    const { lat, lng, type, title, street, number, floor, zoneId, landmark,location} = req.body;
 
     const existingAddress = await db.select().from(addresses).where(eq(addresses.id, addressId)).limit(1);
     if (!existingAddress[0]) {
@@ -71,7 +72,7 @@ export const updateUserAddress = async (req: Request, res: Response) => {
 
     await db
         .update(addresses)
-        .set({ lat, lng, type, title, street, number, floor, zoneId, landmark: landmark ?? null })
+        .set({ lat, lng, type, title, street, number, floor, zoneId, landmark: landmark ?? null, location: location ?? null })
         .where(eq(addresses.id, addressId));
 
     return SuccessResponse(res, { message: "Address updated successfully" });
--- src/models/schema/user/address.ts
+++ src/models/schema/user/address.ts
@@ -9,6 +9,7 @@ import {
 import { sql } from "drizzle-orm";
 import { users ,} from "./Users";
 import {zones} from "../admin/zone"
+
 export const addresses = mysqlTable("addresses", {
     id: char("id", { length: 36 }).primaryKey().default(sql`(UUID())`),
     userId: char("user_id", { length: 36 }).references(() => users.id).notNull(),
@@ -21,4 +22,7 @@ export const addresses = mysqlTable("addresses", {
     number: varchar("number", { length: 20 }).notNull(),
     floor: varchar("floor", { length: 20 }),
     landmark: varchar("landmark", { length: 500 }),
+    location: varchar("location", { length: 255 }),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
 });
