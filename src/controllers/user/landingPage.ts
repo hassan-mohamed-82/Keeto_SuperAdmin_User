5f4ec4ab7023d705303dfd8c5af70a58f80a2197
basantsaed2 <basantsaed718@gmail.com>
2026-08-12 14:48:11 +0300

    edit landing page

--- src/controllers/user/landingPage.ts
+++ src/controllers/user/landingPage.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from "express";
 import { db } from "../../models/connection";
 import { restaurants } from "../../models/schema";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { SuccessResponse } from "../../utils/response";
 
 export const getActiveRestaurants = async (req: Request, res: Response) => {
     try {
         const activeRestaurants = await db.select({
+            id: restaurants.id,
             name: restaurants.name,
             nameAr: restaurants.nameAr,
             nameFr: restaurants.nameFr,
             orderLink: restaurants.orderLink,
             logo: restaurants.logo
         })
-        .from(restaurants)
-        .where(eq(restaurants.status, "active"));
+            .from(restaurants)
+            .where(
+                and(
+                    eq(restaurants.status, "active"),
+                    eq(restaurants.deliverystatus, "delivered")
+                )
+            );
 
         return SuccessResponse(res, {
             message: "Active restaurants fetched successfully",
