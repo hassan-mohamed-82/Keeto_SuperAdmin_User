dbb555bd6556493057a334f111efc5064d2fc3e9
basantsaed2 <basantsaed718@gmail.com>
2026-08-17 13:27:34 +0300

    make api to getRestaurantsBranches

--- src/routes/user/restaurantFeatures.ts
+++ src/routes/user/restaurantFeatures.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { searchRestaurants, toggleAddHome, getHomeRestaurants, removeFromHome, getResturantSchedules } from "../../controllers/user/restaurantFeatures";
+import { searchRestaurants, toggleAddHome, getHomeRestaurants, removeFromHome, getResturantSchedules, getRestaurantsBranches } from "../../controllers/user/restaurantFeatures";
 import { catchAsync } from "../../utils/catchAsync";
 
 const router = Router();
@@ -8,6 +8,8 @@ router.get("/search", catchAsync(searchRestaurants));
 router.get("/home-list", catchAsync(getHomeRestaurants));
 router.put("/:restaurantId/addhome", catchAsync(toggleAddHome));
 router.delete("/:restaurantId/addhome", catchAsync(removeFromHome));
+
+router.get("/:restaurantId/branches", catchAsync(getRestaurantsBranches));
 
 //check if the restaurant is open
 router.get("/resturant-schedules/:restaurantId", catchAsync(getResturantSchedules))
