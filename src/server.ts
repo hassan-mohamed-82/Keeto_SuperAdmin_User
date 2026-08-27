583fd77de2d912088702def636c17cf8fec68b13
basantsaed2 <basantsaed718@gmail.com>
2026-08-27 18:30:18 +0300

    important edit in handleimage and server to fast load image

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import { connectDB } from './models/connection';
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", true);
 connectDB(); 
 
 const httpServer = http.createServer(app);
@@ -45,8 +46,11 @@ app.use(cookieParser());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
-// إعداد المسارات الثابتة (Static Files)
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+// إعداد المسارات الثابتة (Static Files مع التخزين المؤقت)
+app.use("/uploads", express.static(path.join(__dirname, "../uploads"), {
+  maxAge: "30d",
+  immutable: true
+}));
 app.use(express.static(path.join(process.cwd(), "public")));
 
 // اختبار عمل الـ API
