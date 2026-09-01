ea55574e307cfa236dd96bdd195f41f5f294ef21
basantsaed2 <basantsaed718@gmail.com>
2026-09-01 13:33:01 +0300

    edit policy

--- src/controllers/user/policy.ts
+++ src/controllers/user/policy.ts
@@ -1,30 +1,33 @@
 import { catchAsync } from "../../utils/catchAsync";
-import { Response , Request} from "express";
-import {SuccessResponse} from "../../utils/response";
+import { Response, Request } from "express";
+import { SuccessResponse } from "../../utils/response";
 import { policy } from "../../models/schema";
-import { eq , and } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { BadRequest } from "../../Errors";
 import { db } from "../../models/connection";
 
-export const getAllPolicy = async(req:Request,res:Response)=>{
-   const { restaurantId } = req.params;
+export const getAllPolicy = async (req: Request, res: Response) => {
+    const { restaurantId } = req.params;
 
     if (!restaurantId) throw new BadRequest("Please provide restaurant id");
 
-    const [restaurantPolicy] = await db
+    const restaurantPolicies = await db
         .select()
         .from(policy)
         .where(
             and(
-                eq(policy.type,"restaurant"),
+                eq(policy.type, "restaurant"),
                 eq(policy.restaurantId, restaurantId)
             )
         );
 
-    if (!restaurantPolicy) throw new BadRequest("Policy not found");
+    // 🟢 التحقق من طول المصفوفة للتأكد من وجود سياسات
+    if (!restaurantPolicies || restaurantPolicies.length === 0) {
+        throw new BadRequest("Policies not found");
+    }
 
     return SuccessResponse(res, {
-        message:"Policy fetched successfully",
-        data: restaurantPolicy,
+        message: "Policies fetched successfully",
+        data: restaurantPolicies,
     });
 }
