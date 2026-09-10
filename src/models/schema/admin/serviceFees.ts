0cafaa4fe4335e5c6a9967c7551308c0b436152e
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-09-10 14:53:28 +0300

    dfgdf

--- src/models/schema/admin/serviceFees.ts
+++ src/models/schema/admin/serviceFees.ts
@@ -12,12 +12,14 @@ import { restaurants } from "./restaurants";
 
 export type ServiceFeeModule = "take_away" | "dine_in" | "delivery" | "car" | "all";
 export type ServiceFeeType = "web" | "app" | "all";
+export type AmountType = "percentage" | "value";
 
 export const serviceFees = mysqlTable("service_fees", {
     id: char("id", { length: 36 }).primaryKey().default(sql`(UUID())`),
     restaurantId: char("restaurant_id", { length: 36 }).references(() => restaurants.id, { onDelete: "cascade" }),
     name: varchar("name", { length: 255 }),
     amount: decimal("amount", { precision: 10, scale: 2 }).notNull(),
+    amountType: mysqlEnum("amount_type", ["percentage", "value"]).default("percentage").notNull(),
     modules: json("modules").$type<ServiceFeeModule[]>().default(["all"]).notNull(),
     type: mysqlEnum("type", ["web", "app", "all"]).default("all").notNull(),
     branchIds: json("branch_ids").$type<string[]>().default([]).notNull(),
--- src/models/schema/admin/taxes.ts
+++ src/models/schema/admin/taxes.ts
@@ -12,12 +12,14 @@ import { restaurants } from "./restaurants";
 
 export type TaxModule = "take_away" | "dine_in" | "delivery" | "car" | "all";
 export type TaxType = "web" | "app" | "all";
+export type TaxAmountType = "percentage" | "value";
 
 export const taxes = mysqlTable("taxes", {
     id: char("id", { length: 36 }).primaryKey().default(sql`(UUID())`),
     restaurantId: char("restaurant_id", { length: 36 }).references(() => restaurants.id, { onDelete: "cascade" }),
     name: varchar("name", { length: 255 }),
     amount: decimal("amount", { precision: 10, scale: 2 }).notNull(),
+    amountType: mysqlEnum("amount_type", ["percentage", "value"]).default("percentage").notNull(),
     modules: json("modules").$type<TaxModule[]>().default(["all"]).notNull(),
     type: mysqlEnum("type", ["web", "app", "all"]).default("all").notNull(),
     foodIds: json("food_ids").$type<string[]>().default([]).notNull(),
